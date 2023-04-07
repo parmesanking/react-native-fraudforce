c3eff87149f68628fd73768a2e12571e922d2692
Luca Agostini <luca@betcha.com>
2023-04-07 16:15:52 +0200

    upd: ugraded PX sdk for android

--- android/src/main/java/com/reactnativefraudforce/FraudforceModule.kt
+++ android/src/main/java/com/reactnativefraudforce/FraudforceModule.kt
@@ -11,6 +11,7 @@ import com.iovation.mobile.android.FraudForceManager
 import com.perimeterx.mobile_sdk.PerimeterX
 import com.perimeterx.mobile_sdk.PerimeterXDelegate
 import com.perimeterx.mobile_sdk.main.PXPolicy
+import com.perimeterx.mobile_sdk.main.PXPolicyUrlRequestInterceptionType
 
 
 const val PX_MAX_START_ATTEMPTS = 10
@@ -51,28 +52,35 @@ class FraudforceModule(reactContext: ReactApplicationContext) : ReactContextBase
     }
 
     @ReactMethod
-    fun startPerimeterX(appId: String, promise: Promise) {
+    fun startPerimeterX(appId: String, forDomains: ArrayList<String>, promise: Promise) {
       UiThreadUtil.runOnUiThread {
         Log.d("PerimeterX", "Starting PerimeterX..")
+        val policy = PXPolicy()
+        policy.doctorCheckEnabled = false
+        policy.setDomains(forDomains, appId)
+        policy.urlRequestInterceptionType = PXPolicyUrlRequestInterceptionType.NONE
+
+
         if (!this.perimeterXStarted && this.perimeterXStartAttempt< PX_MAX_START_ATTEMPTS) {
-          PerimeterX.INSTANCE.start(this.appContext as Application,  appId, this, false) { success ->
+          try {
+            PerimeterX.INSTANCE.start(this.appContext as Application,  appId, this, policy)
+            Log.d("PerimeterX", "PerimeterX started...")
+
+            this.perimeterXStarted = true
+            promise.resolve(null)
+
+          } catch (error:Exception) {
+            Log.d("PerimeterX", error.message)
+
             this.perimeterXStartAttempt += 1
-            if (this.perimeterXStartAttempt >= PX_MAX_START_ATTEMPTS){
-              promise.reject("PX Start error", "Unable to start PerimeterX", null)
-            }else {
-              if (!success) {
-                // make sure to start the sdk again when it fails (network issue, etc.)
-                Log.d("PerimeterX", "PerimeterX not started...")
-                Thread.sleep(1_000)
-                this.startPerimeterX(appId, promise)
-              } else {
-                Log.d("PerimeterX", "PerimeterX started...")
-                val policy = PXPolicy()
-                policy.requestsInterceptedAutomaticallyEnabled = false
-                PerimeterX.INSTANCE.setPolicy(policy, appId)
-                this.perimeterXStarted = true
-                promise.resolve(null)
-              }
+            if (this.perimeterXStartAttempt >= PX_MAX_START_ATTEMPTS) {
+              promise.reject("PX Start error", "Unable to start PerimeterX", error)
+            } else {
+              // make sure to start the sdk again when it fails (network issue, etc.)
+              Log.d("PerimeterX", "PerimeterX not started...")
+              Thread.sleep(1_000)
+              this.startPerimeterX(appId, forDomains, promise)
+
             }
           }
         }
@@ -100,35 +108,12 @@ class FraudforceModule(reactContext: ReactApplicationContext) : ReactContextBase
   fun handleResponse(response: String?, code: Int?, url: String?, promise: Promise) {
     UiThreadUtil.runOnUiThread {
       try {
-        val isHandledByPX: Boolean = PerimeterX.INSTANCE.handleResponse(null, response!!, code!!)
+        val isHandledByPX: Boolean = PerimeterX.INSTANCE.handleResponse(response!!, null, null)
         promise.resolve(isHandledByPX)
       }catch(e:Exception){
         promise.reject(e)
       }
     }
-  }
-
-
-  override fun perimeterxRequestBlockedHandler(appId: String) {
-    val payload = Arguments.createMap()
-    // Put data to map
-    // Put data to map
-    payload.putString("appId", appId)
-
-    this.reactContext
-      .getJSModule(RCTDeviceEventEmitter::class.java)
-      .emit("onPerimeterXRequestBlocked", payload)
-  }
-
-  override fun perimeterxChallengeSolvedHandler(appId: String) {
-    val payload = Arguments.createMap()
-    // Put data to map
-    // Put data to map
-    payload.putString("appId", appId)
-
-    this.reactContext
-      .getJSModule(RCTDeviceEventEmitter::class.java)
-      .emit("onPerimeterXChallengeSolved", payload)
   }
 
   override fun perimeterxChallengeCancelledHandler(appId: String) {
@@ -142,6 +127,35 @@ class FraudforceModule(reactContext: ReactApplicationContext) : ReactContextBase
       .emit("onPerimeterXChallengeCancelled", payload)
   }
 
+  override fun perimeterxChallengeSolvedHandler(appId: String) {
+    val payload = Arguments.createMap()
+    // Put data to map
+    // Put data to map
+    payload.putString("appId", appId)
+
+    this.reactContext
+      .getJSModule(RCTDeviceEventEmitter::class.java)
+      .emit("onPerimeterXChallengeSolved", payload)
+  }
+
+  override fun perimeterxHeadersWereUpdated(headers: HashMap<String, String>, appId: String) {
+    this.reactContext
+      .getJSModule(RCTDeviceEventEmitter::class.java)
+      .emit("onPerimeterXHeadersUpdated", headers)
+
+  }
+
+  override fun perimeterxRequestBlockedHandler(url: String?, appId: String) {
+    val payload = Arguments.createMap()
+    // Put data to map
+    // Put data to map
+    payload.putString("appId", appId)
+    payload.putString("url", url)
+
+    this.reactContext
+      .getJSModule(RCTDeviceEventEmitter::class.java)
+      .emit("onPerimeterXRequestBlocked", payload)
+  }
 
   @ReactMethod
   fun addListener(eventName: String?) {
